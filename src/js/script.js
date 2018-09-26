0e7d45276e131f85735369445887d9ea413936e9
Matthieu Toussaint <matthieu.toussaint@hetic.net>
2018-09-26 16:20:48 +0200

    collect data from json :wrench:

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
-console.log('hello')
+const requestURL = 'http://localhost:3000/data/all.json';
+const request = new XMLHttpRequest();
+
+const $header = document.querySelector('.header')
+
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = function() {
+  const response = request.response;
+  general(response);
+}
+
+function general(jsonObj) {
+  const $general_title = $header.querySelector('h1.title')
+  $general_title.innerHTML = jsonObj['general'].title
+
+}
