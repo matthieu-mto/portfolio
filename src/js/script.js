62b3e78d2828f33f04fed9962b0471063574f1bb
Matthieu Toussaint <matthieu.toussaint@hetic.net>
2018-09-26 17:05:46 +0200

    add profile picture :art:

--- src/js/script.js
+++ src/js/script.js
@@ -8,15 +8,18 @@ request.responseType = 'json';
 request.send();
 
 request.onload = function() {
-  const response = request.response;
-  general(response);
+    const response = request.response;
+    general(response);
 }
 
 function general(jsonObj) {
-  const $general_title = $header.querySelector('h1.title')
-  $general_title.innerHTML = jsonObj['general'].title
+    const $general_title = $header.querySelector('h1.title')
+    $general_title.innerHTML = jsonObj['general'].title
 
-  const $general_description = $header.querySelector('p.description')
-  $general_description.innerHTML = jsonObj['general'].description
+    const $general_picture = $header.querySelector('img.profile-picture')
+    $general_picture.src = jsonObj['general'].picture
+
+    const $general_description = $header.querySelector('p.description')
+    $general_description.innerHTML = jsonObj['general'].description
 
 }
