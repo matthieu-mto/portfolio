a819ed32daab4245b8bceee2adbd10f26c5a2596
Matthieu Toussaint <matthieu.toussaint@hetic.net>
2018-09-26 17:15:03 +0200

    add new section :heavy_plus_sign:

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,20 @@
-const requestURL = 'http://localhost:3000/data/all.json';
-const request = new XMLHttpRequest();
+const requestURL = 'http://localhost:3000/data/all.json'
+const request = new XMLHttpRequest()
 
-const $header = document.querySelector('.header')
+const $header = document.querySelector('header.header')
+const $about = document.querySelector('section.about')
 
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
+request.open('GET', requestURL)
+request.responseType = 'json'
+request.send()
 
 request.onload = function() {
-    const response = request.response;
-    general(response);
+    const response = request.response
+    section_header(response)
+    section_about(response)
 }
 
-function general(jsonObj) {
+function section_header(jsonObj) {
     const $general_title = $header.querySelector('h1.title')
     $general_title.innerHTML = jsonObj['general'].title
 
@@ -21,5 +23,9 @@ function general(jsonObj) {
 
     const $general_description = $header.querySelector('p.description')
     $general_description.innerHTML = jsonObj['general'].description
+
+}
+
+function section_about(jsonObj) {
 
 }
