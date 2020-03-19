fb0a42a74b2d0b4b8e5828cff9cf073af96cbcf0
matt_mlz <matthieu@matthieutoussaint.fr>
2020-03-19 17:37:14 +0100

    put all queries on front

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,9 +1,52 @@
 <template>
-  <div>test</div>
+  <div>
+    {{ header }}
+    {{ abouts }}
+    {{ skills }}
+    {{ socials }}
+    {{ projects }}
+  </div>
 </template>
 
 <script>
+import headerQuery from '~/apollo/queries/header/header'
+import aboutsQuery from '~/apollo/queries/about/abouts'
+import skillsQuery from '~/apollo/queries/skill/skills'
+import socialsQuery from '~/apollo/queries/social/socials'
+import projectsQuery from '~/apollo/queries/project/projects'
+
 export default {
+  data() {
+    return {
+      header: {},
+      abouts: [],
+      skills: [],
+      socials: [],
+      projects: []
+    }
+  },
+  apollo: {
+    header: {
+      prefetch: true,
+      query: headerQuery
+    },
+    abouts: {
+      prefetch: true,
+      query: aboutsQuery
+    },
+    skills: {
+      prefetch: true,
+      query: skillsQuery
+    },
+    socials: {
+      prefetch: true,
+      query: socialsQuery
+    },
+    projects: {
+      prefetch: true,
+      query: projectsQuery
+    }
+  },
   components: {}
 }
 </script>
