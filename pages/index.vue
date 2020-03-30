c14bf518162f378dd0702f4c4d0d25ec95f4cff4
matt_mlz <matthieu@matthieutoussaint.fr>
2020-03-30 15:09:36 +0200

    remove apollo

--- pages/index.vue
+++ pages/index.vue
@@ -1,54 +1,13 @@
 <template>
   <div>
     TEST
-    {{ header }}
-    {{ abouts }}
-    {{ skills }}
-    {{ socials }}
-    {{ projects }}
   </div>
 </template>
 
 <script>
-import headerQuery from '~/apollo/queries/header/header'
-import aboutsQuery from '~/apollo/queries/about/abouts'
-import skillsQuery from '~/apollo/queries/skill/skills'
-import socialsQuery from '~/apollo/queries/social/socials'
-import projectsQuery from '~/apollo/queries/project/projects'
-
 export default {
   components: {},
-  data() {
-    return {
-      header: {},
-      abouts: [],
-      skills: [],
-      socials: [],
-      projects: []
-    }
-  },
-  apollo: {
-    header: {
-      prefetch: true,
-      query: headerQuery
-    },
-    abouts: {
-      prefetch: true,
-      query: aboutsQuery
-    },
-    skills: {
-      prefetch: true,
-      query: skillsQuery
-    },
-    socials: {
-      prefetch: true,
-      query: socialsQuery
-    },
-    projects: {
-      prefetch: true,
-      query: projectsQuery
-    }
-  }
+  data() {}
 }
 </script>
 
