181841ba9c8beeef539579d7483acd66b249259d
Matthieu GREGORIS TOUSSAINT <matthieu.toussaint20@gmail.com>
2026-08-04 10:35:02 +0200

    clean everything

--- app/app.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="page">
-    <a class="skip-link" href="#main">Skip to content</a>
-    <SiteNav />
-    <NuxtPage />
-    <SiteFooter />
-  </div>
-</template>
--- server/routes/robots.txt.ts
+++
@@ -1,6 +0,0 @@
-import { abs } from '#shared/site'
-
-export default defineEventHandler((event) => {
-  setHeader(event, 'content-type', 'text/plain; charset=utf-8')
-  return `User-agent: *\nAllow: /\n\nSitemap: ${abs('/sitemap.xml')}\n`
-})
